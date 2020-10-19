48c1c05bbb5f9b4917c59b80d234535c56b5d112
limingze <limingze@didiglobal.com>
2020-10-19 15:43:06 +0800

    变量

---
+++ Go/demo/2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var n = "hello"
+
+func main() {
+	var n = "hello2"
+	{
+		var n = "hello3"
+		fmt.Println("hello3:", n)
+	}
+	fmt.Println("hello2:", n)
+
+	//n := "hello2"
+	{
+		n := 1
+		fmt.Println("hello3:", n)
+	}
+	fmt.Println("hello2:", n)
+
+}
